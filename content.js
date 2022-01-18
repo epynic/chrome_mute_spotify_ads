3f126d2556db03cad8be55545c3a73e2bdd2f098
Prasanha Kumar <epynic@gmail.com>
2022-01-18 20:12:51 +0530

    Another lie

--- content.js
+++ content.js
@@ -2,27 +2,35 @@ chrome.runtime.sendMessage({ todo: "showPageAction" });
 chrome.runtime.sendMessage({ muteTab: false })
 
 var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        if (mutation.attributeName === "data-testid") {
-            var attributeValue = mutation.target.dataset.testid;
-            if (!attributeValue) return;
-            if (attributeValue == 'now-playing-bar-ad-type-ad') {
-                chrome.runtime.sendMessage({ muteTab: 'yes' });
-            } else chrome.runtime.sendMessage({ muteTab: 'no' });
-        }
-    });
+    if(mutations[0].target.innerText.indexOf('Advertisement') !== -1)
+        chrome.runtime.sendMessage({ muteTab: 'yes' });
+    else
+        chrome.runtime.sendMessage({ muteTab: 'no' });
+    // mutations.forEach(function (mutation) {
+    //     console.log("This line 6");
+    //     if (mutation.attributeName === "data-testid") {
+    //         var attributeValue = mutation.target.dataset.testid;
+    //         if (!attributeValue) return;
+    //         if (attributeValue == 'now-playing-bar-ad-type-ad') {
+    //             chrome.runtime.sendMessage({ muteTab: 'yes' });
+    //         } else chrome.runtime.sendMessage({ muteTab: 'no' });
+    //     }
+    // });
 });
 
 function isNodeAvailable() {
-    var playFooter = document.querySelectorAll(".now-playing-bar-container")[0];
-    if (!playFooter) {
+    console.log('isNodeAvailable');
+
+    var target = document.querySelector('title');
+    if(!target) {
         window.setTimeout(isNodeAvailable, 500);
         return;
     }
 
-    observer.observe(playFooter, {
-        attributes: true
+    observer.observe(target, {
+        subtree: true, characterData: true, childList: true 
     });
+
 }
 
 isNodeAvailable();
